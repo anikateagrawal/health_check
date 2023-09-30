a11d96a4359fddf7d43b9de18440c2bb0ff89c20
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-09-30 20:22:18 +0530

    commit

--- src/components/Symptom.jsx
+++ src/components/Symptom.jsx
@@ -6,11 +6,13 @@ const Symptom = (props) => {
     const {data}=useContext(symptoms);
   return (
     <div className='symptom'>
+      <div>
         <label htmlFor={props.val}>Select Symptom{props.val+1} : </label>
         <select name={props.val} id={props.val} defaultValue="none">
           <option value="none">None</option>
           {data.map((sym)=><option value={sym}>{sym}</option>)}
         </select>
+        </div>
     </div>
   )
 }
